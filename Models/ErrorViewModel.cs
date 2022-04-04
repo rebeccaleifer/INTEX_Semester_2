9af38049c68c006b728e3f2a64675eace58ada41
rebeccaleifer <rebecca.leifer15@gmail.com>
2022-04-04 10:16:06 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Intex_Semester_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
